8417e78ad61c89336862f84c93b58a609e91eb4d
h0996g <h0996g@gmail.com>
2024-06-19 11:21:06 +0100

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,153 @@
+import 'package:e_learn2/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'components/appbar.dart';
+import 'components/category.dart';
+import 'components/sorting.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //bottom bar
+      // now we will use bottom bar package
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_rounded),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: kpink,
+        unselectedItemColor: Colors.grey[300],
+      ),
+
+      body: SafeArea(
+        child: ListView(
+          children: [
+            CustomeAppBar(),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Hi Julia",
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            "Today is a good day\nto learn something new!",
+                            style: TextStyle(
+                              color: Colors.black54,
+                              wordSpacing: 2.5,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        children: [
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                                color: kpurple,
+                                borderRadius: BorderRadius.circular(15.0)),
+                            child: Image.asset(
+                              "assets/images/profile.png",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //sorting
+                  Sorting(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //category list
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Categories",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          "See All",
+                          style: TextStyle(fontSize: 16, color: kblue),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //now we create model of our images and colors which we will use in our app
+                  const SizedBox(
+                    height: 20,
+                  ),
+//we can not use gridview inside column
+//use shrinkwrap and physical scroll
+                  CategoryList(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
